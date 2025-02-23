41bbf4b35efffcf7a8e06b374fa341eb0c5acc62
Jonath Oliveira <akiles22a@gmail.com>
2025-02-23 13:12:14 -0300

    criacao do projeto convite +

---
+++ conviteplus/src/main/java/com/conviteplus/conviteplus/ConviteplusApplication.java
@@ -0,0 +1,13 @@
+package com.conviteplus.conviteplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConviteplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConviteplusApplication.class, args);
+	}
+
+}
